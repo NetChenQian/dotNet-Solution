99a59d8f4a19b9066aa6399481d6adc2ad8f5072
ChenQian <1576217157@qq.com>
2017-07-03 16:32:13 +0800

    Initilaztion and Condition Breakpoint!

---
+++ ConditionBreakpoint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/// <summary>
+/// Condition Breakpoint
+/// </summary>
+namespace ConditionBreakpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                //while (i-1)%10==0 and i>10, print i and sum
+                sum += i;
+            }
+            Console.WriteLine("Sum: "+sum);
+            Console.ReadLine();
+        }
+    }
+}
